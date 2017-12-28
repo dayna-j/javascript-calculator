2b379c39ca64155002ff1cd79709420485e92e38
Dayna Blackwell <daynajblackwell@gmail.com>
2017-12-27 23:37:39 -0700

    Beginning to model objects with JavaScript.

---
+++ javascript-calculator.js
@@ -0,0 +1,34 @@
+// on numerical buttons, when button is clicked add value to
+// inputArray
+
+function Calculator()
+{
+	this._inputArray = [];
+	this._display = '';
+	this.appendDisplay = function()
+	{
+		
+	}
+	
+//	Object.defineProperty(this, "appendDisplay"),
+//		{}
+}
+
+function appendInputArray()
+{
+	var temp = $('#7').val();
+	
+}
+
+//function appendDisplay(value)
+//{
+//	
+//}
+
+
+
+$(document).ready(function()
+{
+	var calc = new Calculator();
+	
+});
