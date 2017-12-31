19e55602cd6ce5568bba02a14f89f37cb858f5c6
Dayna Blackwell <daynajblackwell@gmail.com>
2017-12-30 23:20:56 -0700

    misc updates

--- javascript-calculator.js
+++ javascript-calculator.js
@@ -1,24 +1,31 @@
+//function keyPress(){alert('key pressed');}
 
 function Calculator()
 {
+	alert("calc created");
 	this._inputArray = [];
 	this._displayResult = '';
 	this._displayEquation = '';
-	this.keyPress = function(){
-	// What happens when a numKey is pressed?  
-	// 1) Find out what kind of key it was.  num/admin/op
-	//		look at class html attribute:  [numKey, adminKey, opKey]
-		alert("keyPress is working");
-	}
-	
-	
-	this.appendDisplay = function()
+	this.keyPress = function()
 	{
-		
+		var pressedKey;
+		// What happens when a numKey is pressed?  
+		// 1) Find out what kind of key it was.  num/admin/op
+		//		look at class html attribute:  [numKey, adminKey, 
+		//		opKey]
+		alert("keyPress is working");
+			
 	}
 }
 
 $(document).ready(function()
 {
+	alert('dom is ready.');
 	var calc = new Calculator();
-})
+//	$(".adminKey").on("click",function(){calc.keyPress();});
+//	$(".numKey").on("click",function(){calc.keyPress();});
+//	$(".opKey").on("click",function(){calc.keyPress();});
+	$(".Key").on("click",function(){calc.keyPress();});
+	
+	
+});
