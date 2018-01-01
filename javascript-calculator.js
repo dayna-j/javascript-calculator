5e6690c184aece1fa594cd362bac789e7f8b6de1
Dayna Blackwell <daynajblackwell@gmail.com>
2017-12-31 21:31:29 -0700

    keyPress method detects buttons by ID now

--- javascript-calculator.js
+++ javascript-calculator.js
@@ -1,82 +1,127 @@
+$(document).ready(function()
+{
+
 function Calculator()
 {
+	alert("calc object created");
+	// OPERAND PROPERTIES
+
 	// this._operandOne = '';
 	this._operandOne = '';
 	this._operandTwo = '';
 
-	alert("calc created");
 	this._inputArray = [];
 	this._displayResult = $("#displayResult");
 	this._displayEquation = $("#displayEquation");
+	
+	// BUTTON PROPERTIES
+	
+	this.numKeys = $(".numKey");
+	this.adminKeys = $(".adminKey");
+	this.opKeys = $(".opKeys");
+	this.keys = $(".Key");
 
-	this.keyPress = function()
-	{
-		var pressedKey;
-		// What happens when a numKey is pressed?  
-		// 1) Find out what kind of key it was.  num/admin/op
-		//		look at class html attribute:  [numKey, adminKey, 
-		//		opKey]
-		alert("keyPress is working");
+	// METHODS
+	
+	this.keyPress = function(event)
+    {
+        // What happens when a numKey is pressed?
+        // 1) Find out what kind of key it was.  num/admin/op
+        //		look at class html attribute:  [numKey, adminKey,
+        //		opKey]
+
+        // The event.target property returns which DOM element triggered the event.
+        var pressedKey = event.target;
+        alert(pressedKey.id);
+        console.log(typeof pressedKey.id);
+
+        
+
+        // return pressedKey.id;
+
+
 	}
+
+	this.updateResultScreen = function(key)
+	{
+		this.setResultScreen = this.getResultScreen + key;
+	}
+
 }
 
-// Object.defineProperty(calc, "getResultScreen",
-// 	{
-// 	get:
-// 	function()
-// 	{
-// 		return this._displayResult;
-// 		alert("_displayResult");
-// 	}}
-//
-// );
-//
-// Object.defineProperty(Calculator, "setResultScreen",
-//     {
-//         set:
-//             function(screen)
-//             {
-//                 this._displayResult = screen;
-//             }}
-//
-// );
-//
-// Object.defineProperty(Calculator, "getEquationScreen",
-//     {
-//         get:
-//             function()
-//             {
-//                 return this._displayEquation;
-//             }}
-//
-// );
-//
-// Object.defineProperty(Calculator, "setEquationScreen",
-//     {
-//         set:
-//             function(screen)
-//             {
-//                 this._displayEquation = screen;
-//             }}
-//
-// );
+var calc = new Calculator();
 
-$(document).ready(function()
-{
-	alert('dom is ready.');
-	var calc = new Calculator();
+Object.defineProperty(calc, "getResultScreen",
+ 	{
+ 	get:
+ 		function()
+ 		{
+ 			return this._displayResult;
+ 			alert(_displayResult);
+ 		}
+ 	}
+ );
 
-	var numKeys = $(".numKey");
-	var opKeys = $(".opKey");
-	var adminKeys = $(".adminKey");
+ Object.defineProperty(calc, "setResultScreen",
+     {
+         set:
+             function(screen)
+             {
+                 this._displayResult = screen;
+             }
+     }
+ );
 
-	// calc.setResultScreen =  $("#displayResult");
-	// calc.setEquationScreen = $("#displayEquations");
-	// calc.getResultScreen();
+ Object.defineProperty(calc, "getEquationScreen",
+     {
+         get:
+             function()
+             {
+                 return this._displayEquation;
+             }
+     }
+ );
 
-	
-//	$(".adminKey").on("click",function(){calc.keyPress();});
-//	$(".numKey").on("click",function(){calc.keyPress();});
-//	$(".opKey").on("click",function(){calc.keyPress();});
-	$(".Key").on("click",function(){calc.keyPress();});
+ Object.defineProperty(calc, "setEquationScreen",
+     {
+         set:
+             function(screen)
+             {
+                 this._displayEquation = screen;
+             }
+     }
+ );
+
+$(".Key").on("click",function(){calc.keyPress(event);});
+
+console.dir(calc.keys[0]);
+console.dir(calc.getResultScreen);
+
+
+
 });
+
+
+//
+// $(document).ready(function()
+// {
+// 	alert('dom is ready.');
+// //	var calc = new Calculator();
+//
+// 	// var numKeys = $(".numKey");
+// 	// var opKeys = $(".opKey");
+// 	// var adminKeys = $(".adminKey");
+// 	// var keys = $('.Key');
+//
+// 	// calc.setResultScreen =  $("#displayResult");
+// 	// calc.setEquationScreen = $("#displayEquations");
+// 	// calc.getResultScreen();
+//
+//
+// //	$(".adminKey").on("click",function(){calc.keyPress();});
+// //	$(".numKey").on("click",function(){calc.keyPress();});
+// //	$(".opKey").on("click",function(){calc.keyPress();});
+//
+// 	$(".Key").on("click",function(){calc.keyPress();});
+// 	console.log(calc.keys[1]);
+// });
